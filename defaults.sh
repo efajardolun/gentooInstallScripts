8baf4f5175ec5f6a8775f0e12f5a483d35a6ab5b
Eduardo Fajardo <efajardol@unal.edu.co>
2022-12-20 16:36:01 -0500

    the scripts was changed to unattended mode

---
+++ defaults.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+DEFAULT_IP=192.168.100.69/24
+DEFAULT_HOSTNAME=gentoo_installing
+DEFAULT_ROUTE=192.168.100.1
+DEFAULT_NAMESERVER=168.176.5.148
+DEFAULT_LINK=https://bouncer.gentoo.org/fetch/root/all/releases/amd64/autobuilds/20221218T170210Z/stage3-amd64-openrc-20221218T170210Z.tar.xz
